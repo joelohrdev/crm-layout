358aa2a3213c5375aeca4c204b44502398d59914
joelohrdev <emailme@joelohr.com>
2022-09-08 15:13:03 -0500

    client show smaller font and shadows

--- resources/views/livewire/client-show.blade.php
+++ resources/views/livewire/client-show.blade.php
@@ -11,7 +11,7 @@
         </div>
     </div>
     <div class="grid grid-cols-1 gap-16 lg:grid-flow-col-dense lg:grid-cols-3">
-       <div class="lg:col-span-1 bg-gray-50 p-5">
+       <div class="lg:col-span-1 bg-gray-50 p-5 shadow-xl">
            <div class="space-y-10">
                @if($client->servers->count() > 0)
                    <div>
@@ -19,8 +19,8 @@
                        <ul class="divide-y divide-gray-200">
                            @foreach($client->servers as $server)
                                <li class="py-4">
-                                   <a class="w-full flex justify-between items-end" target="_blank" href="https://{{ $server->ip_address }}:2086">
-                                       {{ $server->name }} <span class="text-gray-400 text-sm">{{ $server->ip_address }}</span>
+                                   <a class="w-full text-sm flex justify-between items-end" target="_blank" href="https://{{ $server->ip_address }}:2086">
+                                       {{ $server->name }} <span class="text-gray-400">{{ $server->ip_address }}</span>
                                    </a>
                                </li>
                            @endforeach
@@ -31,17 +31,17 @@
                    <div>
                        <h2 class="text-xl font-semibold text-tkd-blue-800">Domains</h2>
                        @foreach($client->domains as $domain)
-                           <a target="_blank" href="{{ $domain->url }}">{{ $domain->name }}</a>
+                           <a class="text-sm" target="_blank" href="{{ $domain->url }}">{{ $domain->name }}</a>
                        @endforeach
                    </div>
                @endif
            </div>
        </div>
-        <div class="lg:col-span-2 bg-gray-50 p-5">
+        <div class="lg:col-span-2 bg-gray-50 p-5 shadow-xl">
             <h2 class="text-xl font-semibold text-tkd-blue-800 mb-5">Contacts</h2>
             <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
                 @foreach($client->contacts as $contact)
-                    <div class="bg-gray-100 p-5">
+                    <div class="bg-gray-100 p-5 text-sm">
                         {{ $contact->first_name }} {{ $contact->last_name }}<br />
                         <span class="text-sm text-gray-400">{{ $contact->position }}</span><br /><br />
                         {{ preg_replace('~.*(\d{3})[^\d]{0,7}(\d{3})[^\d]{0,7}(\d{4}).*~', '($1) $2-$3',$contact->phone_number) }} @if($contact->extension)ext:{{ $contact->extension }}@endif<br />
