1c61d79be31de83f940a45a1ec149d8a7baa1aed
joelohrdev <emailme@joelohr.com>
2022-09-09 15:34:12 -0500

    Updated domain pagination count

--- app/Http/Livewire/DomainsIndex.php
+++ app/Http/Livewire/DomainsIndex.php
@@ -23,7 +23,7 @@ class DomainsIndex extends Component
             'domains' => Domain::with('server')
                 ->search('name', $this->search)
                 ->orderBy('name', 'ASC')
-                ->paginate(1)
+                ->paginate(10)
         ]);
     }
 }
