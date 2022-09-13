074f1c93c5e96827e8244f534670cd9b8b935029
jltkd <jl@turnkeydigital.com>
2022-09-13 13:25:36 -0500

    Message if no expired domains

--- resources/views/livewire/expired-domain-list.blade.php
+++ resources/views/livewire/expired-domain-list.blade.php
@@ -1,7 +1,9 @@
 <div>
     <ul class="space-y-1">
-        @foreach($domains as $domain)
+        @forelse($domains as $domain)
             <li class="text-sm text-slate-500">{{ $domain->name }} - {{ Carbon\Carbon::parse($domain->expires)->format('F d, Y') }}</li>
-        @endforeach
+        @empty
+            <li>There are no expired domains</li>
+        @endforelse
     </ul>
 </div>
