65d1bb965fe2107a14ecfbd2bb8119378809329c
jltkd <jl@turnkeydigital.com>
2022-09-14 15:23:52 -0500

    Expired domain list card for home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,7 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div>
-                <h1 class="font-black text-3xl mb-5">Expired Domains</h1>
+            <div class="grid grid-cols-3 gap-5">
                 <livewire:expired-domain-list />
             </div>
         </div>
--- resources/views/livewire/expired-domain-list.blade.php
+++ resources/views/livewire/expired-domain-list.blade.php
@@ -1,9 +1,29 @@
 <div>
-    <ul class="space-y-1">
-        @forelse($domains as $domain)
-            <li class="text-sm text-slate-500">{{ $domain->name }} - {{ Carbon\Carbon::parse($domain->expires)->format('F d, Y') }}</li>
-        @empty
-            <li>There are no expired domains</li>
-        @endforelse
-    </ul>
+    <div class="overflow-hidden bg-white sm:rounded-lg sm:shadow">
+
+        <div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+            <h3 class="text-lg font-medium leading-6 text-gray-900">Expired Domains</h3>
+        </div>
+
+        <ul role="list" class="divide-y divide-gray-200">
+
+            @forelse($domains as $domain)
+                <li>
+                    <a href="{{ $domain->url }}" target="_blank" class="block hover:bg-gray-50">
+                        <div class="px-4 py-4 sm:px-6">
+                            <div class="flex items-center justify-between">
+                                <div class="truncate text-sm font-medium text-slate-600">{{ $domain->name }}</div>
+                                <div class="ml-2 flex flex-shrink-0">
+                                    <div class="truncate text-sm text-red-600">{{ Carbon\Carbon::parse($domain->expires)->format('F d, Y') }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+            @empty
+
+            @endforelse
+
+        </ul>
+    </div>
 </div>
